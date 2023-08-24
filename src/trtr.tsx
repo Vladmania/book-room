34d0dde0bc55c9a32ed26eed429a864fc282f297
Vlados <du_biz@mail.ru>
2023-08-24 19:58:11 +0300

    Add fanc

--- src/trtr.tsx
+++ src/trtr.tsx
@@ -11,28 +11,99 @@ import { Portal } from '../portal/Portal'
 
 export const Cart = () => {
   const dispatch = useAppDispatch();
-  const isAuts = useAppSelector((state) => state.profil.isAuts);
-  const productsInCart = useAppSelector((state) => state.cart.product);
-  const SuccessfulPurchase = useAppSelector((state) => state.cart.successfulPurchase);
 
+  const productsInCart = useAppSelector((state) => state.cart.product);
+
+  const dispatch = useAppDispatch();
+  const profils = useAppSelector((state) => state.profil.profil);
+  const isAuts = useAppSelector((state) => state.profil.isAuts);
+  const [changeInfo, setChangeInfo] = useState(Boolean);
+  const [error, setError] = useState(Boolean);
+  const [changePassword, setChangePassword] = useState(Boolean);
+  const [name, setName] = useState(String);
+  const [email, setEmail] = useState(String);
+  const [oldPassword, setOldPassword] = useState(String);
+  const [newPassword, setNewPassword] = useState(String);
+  const [replayPassword, setReplayPassword] = useState("");
 
   if (!isAuts) {
     dispatch(openModal(true));
     return <Navigate to="/"></Navigate>;
   }
+
+  const handleFileInput = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    const data = new FormData();
+    data.append("avatar", file);
+    data.append("id", String(profils[0].id));
+    dispatch(thankPutPhoto(data));
+  };
+  const handleChangePassword = () => {
+    if (changeInfo && changePassword) {
+      if (newPassword === replayPassword) {
+        setError(false);
+        dispatch(
+          thankEditorPasswordUser({
+            token: profils[0].token,
+            oldPassword,
+            newPassword,
+          })
+        );
+        dispatch(
+          thankEditorDataUser({
+            token: profils[0].token,
+            name: name || profils[0].name,
+            email: email || profils[0].email,
+          })
+        );
+        setChangeInfo(false);
+        setChangePassword(false);
+      } else {
+        setError(true);
+      }
+    } else if (changeInfo) {
+      dispatch(
+        thankEditorDataUser({
+          token: profils[0].token,
+          name: name || profils[0].name,
+          email: email || profils[0].email,
+        })
+      );
+      setChangeInfo(false);
+    } else if (changePassword) {
+      if (newPassword === replayPassword) {
+        setError(false);
+        dispatch(
+          thankEditorPasswordUser({
+            token: profils[0].token,
+            oldPassword,
+            newPassword,
+          })
+        );
+        setChangePassword(false);
+      } else {
+        setError(true);
+      }
+    }
+  };
+
+  if (!isAuts) {
+
+    return <Navigate to="/"></Navigate>;
+  }
   const addProductInCart = productsInCart.map((e) => (
     <ProductInCart
-      key={e.id}
+
       id={e.id}
-      name={e.name}
-      autor={e.autor}
-      cover={e.cover}
+
+
       price={e.price}
-      quantity={e.quantity}
+
     />
   ));
   const totalCost = productsInCart.map((e) => e.price * e.quantity);
-  const sum = productsInCart.length !== 0 ? totalCost.reduce((a, b) => a + b) : 0;
+
 
   return (
     <>
